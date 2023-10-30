a14901207b00acc0e4e0ecfc326dc86ed89045e9
sergen aktaş <srgn_akts@hotmail.com>
2023-10-30 11:50:54 +0300

    Add project files.

---
+++ Sergen_Aktas_Odev6/Class/TableCreator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sergen_Aktas_Odev5.Class
+{
+    public class TableCreator
+    {
+        public TableCreator(int width, int height, int rows, int columns)
+        {
+            CreateAndDisplayTable(width, height, rows, columns);
+        }
+
+        private static void CreateAndDisplayTable(int width, int height, int rows, int columns)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                Console.Write("+");
+                for (int j = 0; j < columns; j++)
+                {
+                   
+                    for (int k = 0; k < width; k++)
+                    {
+                        Console.Write("-");
+                    }
+                    Console.Write("+");
+                }
+                Console.WriteLine();
+                for (int m = 0; m < height; m++)
+                {
+                    Console.Write("|");
+                    for (int l = 0; l < columns; l++)
+                    {
+                        for (int n = 0; n < width; n++)
+                        {
+                            Console.Write(" ");
+                        }
+                        Console.Write("|");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.Write("+");
+            for (int j = 0; j < columns; j++)
+            {
+
+                for (int k = 0; k < width; k++)
+                {
+                    Console.Write("-");
+                }
+                Console.Write("+");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+        
+
---
+++ Sergen_Aktas_Odev6/Program.cs
@@ -0,0 +1,60 @@
+﻿using Sergen_Aktas_Odev5.Class;
+
+namespace Sergen_Aktas_Odev6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKey yesNo = ConsoleKey.E;
+            while (yesNo == ConsoleKey.E)
+            {
+                Console.Write("Satır(Row) ve sütun(Column) değeri giriniz (Örn: 2x4): ");
+                string[] input = Console.ReadLine().Split('x');
+                try
+                {
+                    int rows = Convert.ToInt32(input[0]);
+                    int columns = Convert.ToInt32(input[1]);
+                    if (rows <= 27 && columns <= 70 && rows >= 0 && columns >= 0)
+                    {
+                        if (rows == 0 || columns == 0)
+                        {                
+                            yesNo = ReadYesNoKey();
+                        }
+                        else
+                        {
+                            Console.Write("Satır sutun genişliği(Width) ve Yükseliği(Height) değeri giriniz (Örn: 2x4): ");
+                            string[] sizeInput = Console.ReadLine().Split('x');
+                            int width = Convert.ToInt32(sizeInput[0]);
+                            int height = Convert.ToInt32(sizeInput[1]);
+
+                            if (width <= 209/columns && height <= 53/rows && rows >= 0 && columns >= 0)
+                            {
+                                TableCreator tableCreator = new TableCreator(width, height, rows, columns);
+                                yesNo = ReadYesNoKey();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.Clear();
+                    }
+                    Console.Clear();
+                }
+                catch (Exception ex){}
+
+                Console.Clear();
+            }
+        }
+        private static ConsoleKey ReadYesNoKey()
+        {
+            Console.Write("devam etmek için 'E', çıkış için 'H' tuşuna basın.");
+            ConsoleKey check = Console.ReadKey(true).Key;
+            while (check != ConsoleKey.E && check != ConsoleKey.H)
+            {
+                check = Console.ReadKey(true).Key;
+            }
+            return check;
+        }
+    }
+}
